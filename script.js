597e03ac12ac0c75417eca815746a841d060440b
Boris <malikovicboris85@gmail.com>
2022-02-18 00:14:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Show modal window
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', showModal);
+}
+
+// Close modal window
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
